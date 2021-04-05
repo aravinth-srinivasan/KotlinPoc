585e55acd8066b67889b5ef15184e65774dea510
aravinth-srinivasan <aravinth.srinivasan@raweng.com>
2021-04-05 18:23:29 +0530

    flow component added for parallel request

--- app/src/main/java/com/raweng/kotlinpoc/view/todo/ToDoActivity.kt
+++ app/src/main/java/com/raweng/kotlinpoc/view/todo/ToDoActivity.kt
@@ -1,4 +1,5 @@
 package com.raweng.kotlinpoc.view.todo
+
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
@@ -6,30 +7,56 @@ import com.raweng.kotlinpoc.R
 import com.raweng.kotlinpoc.utils.Resource
 import com.raweng.kotlinpoc.view.todo.viewModel.ToDoViewModel
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ToDoActivity : DaggerAppCompatActivity() {
 
     @Inject
-    lateinit var viewModel:ToDoViewModel
+    lateinit var viewModel: ToDoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        for (i in 1..5){
+        /*for (i in 1..5) {
             viewModel.getToDoResponse(i).observe(this, Observer {
-            when(it){
-                is Resource.Success -> {
-                    Log.e("TAG", "onCreate: Data Success")
+                when (it) {
+                    is Resource.Success -> {
+                        Log.e("TAG", "onCreate: Data Success")
+                    }
+                    is Resource.Loading -> {
+                        Log.e("TAG", "onCreate: Data Loading")
+                    }
+                    is Resource.Error -> {
+                        Log.e("TAG", "onCreate: Data Error")
+                    }
                 }
-                is Resource.Loading -> {
-                    Log.e("TAG", "onCreate: Data Loading")
-                }
-                is Resource.Error -> {
-                    Log.e("TAG", "onCreate: Data Error")
+            })
+
+        }*/
+
+        for (i in 1..5) {
+
+            CoroutineScope(Dispatchers.IO).launch {
+                viewModel.getToDoResponseFlow(i).collect {
+                    when (it) {
+                        is Resource.Success -> {
+                            Log.e("TAG", "onCreate: Data Flow Success")
+                        }
+                        is Resource.Loading -> {
+                            Log.e("TAG", "onCreate: Data Flow Loading")
+                        }
+                        is Resource.Error -> {
+                            Log.e("TAG", "onCreate: Data Flow Error")
+                        }
+                    }
                 }
             }
-        })
         }
+
+
     }
 }
--- app/src/main/java/com/raweng/kotlinpoc/view/todo/viewModel/ToDoViewModel.kt
+++ app/src/main/java/com/raweng/kotlinpoc/view/todo/viewModel/ToDoViewModel.kt
@@ -3,8 +3,10 @@ package com.raweng.kotlinpoc.view.todo.viewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.raweng.kotlinpoc.utils.Resource
+import com.raweng.kotlinpoc.view.todo.model.ToDoResponse
 import com.raweng.kotlinpoc.view.todo.repository.ToDoRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class ToDoViewModel @Inject constructor(private val toDoRepository: ToDoRepository):ViewModel() {
@@ -13,6 +15,12 @@ class ToDoViewModel @Inject constructor(private val toDoRepository: ToDoReposito
     fun getToDoResponse(id:Int) = liveData(Dispatchers.IO) {
         emit(Resource.Loading())
         emit(toDoRepository.getToDoResponse(id))
+    }
+
+
+    fun getToDoResponseFlow(id:Int)= flow {
+        emit(Resource.Loading<ToDoResponse>())
+        emit(Resource.Success(toDoRepository.getToDoResponse(id)))
     }
 
 
